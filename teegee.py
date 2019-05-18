742c9f5ddf9ee9ccb3dea1ff594ae27494e13042
Dylan Smyth <dylan@admin-dell.admin-dell>
2019-05-18 12:23:03 +0100

    Added number range option

--- teegee.py
+++ teegee.py
@@ -15,6 +15,7 @@ def main():
   append = "" 
   groupSize = 0
   divider = " "
+  numRange = 0
 
   try:
     if("-t" in sys.argv):
@@ -40,6 +41,8 @@ def main():
       groupSize = int(sys.argv[sys.argv.index("-g")+1])
     if("-d" in sys.argv):
       divider = sys.argv[sys.argv.index("-d")+1]
+    if("-z" in sys.argv):
+      numRange = sys.argv[sys.argv.index("-z")+1]
 
     if(groupSize == 0):
       groupSize = tokenLength
@@ -48,27 +51,33 @@ def main():
       printHelp()
       exit(0) 
       
-  if(selection == ""):
+  if(selection == "" and numRange == 0):
     printHelp()
     exit(0)
   
   for i in range(0, int(repeat)):
-      print(preappend + generateToken(selection, tokenLength, groupSize, divider) + append)
+      print(preappend + generateToken(selection, tokenLength, groupSize, divider, numRange) + append)
 
-def generateToken(alph, length, groupSize, div):
+def generateToken(alph, length, groupSize, div, numRange):
   token = ""
   t = 0
   for i in range(0, int(length)):
     if(t == groupSize):
        t = 0
        token = token + div
-    token = token + alph[randint(0,len(alph)-1)]
+    if(numRange is not 0):
+      token = token + str(getRangedNumber(numRange))
+    else:
+      token = token + alph[randint(0,len(alph)-1)]
     t+=1
 
   return token
 
+def getRangedNumber(numRange):
+  return randint(int(numRange.split("-")[0]), int(numRange.split("-")[1]))
+  
 def printHelp():
-  print("TeeGee: Token Generator\n\n-t\tLength of token\n-l\tInclude lowercase letters\n-u\tInclude uppercase letters\n-n\tInclude numbers\n-s\tInclude symbols(!\"$*(){}[]<>~)\n-c\tInclude custom characters\n-r\tRepeat x times\n-p\tPreappend to token\n-a\tAppend to token\n-g\tSeperate token into groups of x size\n-d\tDivider for groups (Default is a space)")
+  print("TeeGee: Token Generator\n\n-t\tLength of token\n-l\tInclude lowercase letters\n-u\tInclude uppercase letters\n-n\tInclude numbers\n-s\tInclude symbols(!\"$*(){}[]<>~)\n-c\tInclude custom characters\n-r\tRepeat x times\n-p\tPreappend to token\n-a\tAppend to token\n-g\tSeperate token into groups of x size\n-d\tDivider for groups (Default is a space)\n-z\tNumber range")
   print("\nExample usage:\n1) teegee.py -l -u -n -t 13 -c %#=+\n2) teegee.py  -l -u -t 5\n3) teegee.py -t 4 -n")
 if __name__=='__main__':
   main()
